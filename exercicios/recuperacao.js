dec699d7b95d5907d8fe8c10461998d0dd8e5e50
ILTON JUNIO CAETANO DE SOUZA <129558260+iltonjunio@users.noreply.github.com>
2024-02-29 09:54:17 -0300

    up

---
+++ exercicios/recuperacao.js
@@ -0,0 +1,16 @@
+const prompt =require("prompt-sync")()
+
+const nota1= Number (prompt("informe a nota 1 do aluno: "))
+const nota2 = Number (prompt("informe a nota 2 do aluno: "))
+
+const media =  (nota1 + nota2) / 2
+
+if  (media >= 7.0 ) {
+  console.log ("Aprovado")
+}
+else if (media >= 6.0 && media < 7){
+  console.log ("Recupeção")
+}
+else {
+  console.log( "reprovado")
+}
